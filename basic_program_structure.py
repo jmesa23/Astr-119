791fc91305fedc7cb92fc95d5818f03e86363366
jmesa23 <91861328+jmesa23@users.noreply.github.com>
2021-10-07 14:58:20 -0700

    Add files via upload

---
+++ basic_program_structure.py
@@ -0,0 +1,15 @@
+import NumPy as np
+
+
+def main():
+# do some stuff
+
+#rest of the program continues from here
+
+#if the main() function exists, run it
+ if__name__=="__main"
+main()
+
+    #you can do other stuff down here
+
+py-3 pip3 install NumPy
